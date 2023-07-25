aedd5febcd38d5cf54720b84a18345d8d1a7b395
ragan <raganalemu3@gmail.com>
2023-07-25 13:43:45 +0300

    Create associations between classes

--- student.rb
+++ student.rb
@@ -3,9 +3,16 @@ require './person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(name, age, parent_permission, classroom)
+  def initialize(age, classroom, name = 'unknown', parent_permission: true)
     super(name, age, parent_permission)
+    @classroom = classroom # Set the classroom attribute for the student
+    classroom.students << self # Add the student to the students array of the corresponding classroom
+  end
+
+  #   Create the belongs-to side (a student belongs to a classroom).
+  def classroom=(classroom)
     @classroom = classroom
+    classroom.students.push(self) unless classroom.students.includes?(self)
   end
 
   def play_hooky
